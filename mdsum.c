ee9be2c6d8e2aa29d0cfc93b5fd68d48bd0ce10b
Tom Sullivan <tom@msbit.com.au>
2022-06-23 17:36:42 +1000

    Pass deferral continuation

--- mdsum.c
+++ mdsum.c
@@ -1,75 +1,111 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+typedef int (*defer_t)(FILE*);
+
+bool get_size(FILE *, int *, defer_t);
+bool get_rom_end(FILE *, int, int32_t *, defer_t);
+bool get_sum(FILE *, int32_t, uint16_t *, defer_t);
 
 int main(int argc, char **argv) {
-  int result = 0;
   if (argc < 2) {
     fprintf(stderr, "usage: %s <filename>\n", argv[0]);
-    result = 1;
-    goto defer_none;
+    return 1;
   }
 
   FILE *rom = fopen(argv[1], "r");
   if (rom == NULL) {
     perror("fopen");
-    result = 1;
-    goto defer_none;
+    return 1;
   }
 
-  if (fseek(rom, 0, SEEK_END) != 0) {
-    perror("fseek 1");
-    result = 1;
-    goto defer_close_rom;
-  }
-
-  int size = ftell(rom);
-  if (size == -1) {
-    perror("ftell");
-    result = 1;
-    goto defer_close_rom;
-  }
-
-  if (size < 0x1a8) {
-    fprintf(stderr, "error: size 0x%x is less than 0x1a8\n", size);
-    result = 1;
-    goto defer_close_rom;
-  }
-
-  if (fseek(rom, 0x1a4, SEEK_SET) != 0) {
-    perror("fseek 2");
-    result = 1;
-    goto defer_close_rom;
+  int size;
+  if (!get_size(rom, &size, fclose)) {
+    return 1;
   }
 
   int32_t rom_end;
-  if (fread(&rom_end, 1, 4, rom) < 4) {
-    perror("fread");
-    result = 1;
-    goto defer_close_rom;
+  if (!get_rom_end(rom, size, &rom_end, fclose)) {
+    return 1;
   }
 
-  rom_end = ntohl(rom_end) + 1;
-  if (rom_end > size) {
-    fprintf(stderr, "error: rom end 0x%x is beyond size 0x%x\n", rom_end, size);
-    result = 1;
-    goto defer_close_rom;
-  }
-
-  uint16_t sum = 0;
-  for (int32_t i = 0x200; i < rom_end; i += 2) {
-    uint16_t word;
-    fseek(rom, i, SEEK_SET);
-    fread(&word, 1, 2, rom);
-    word = ntohs(word);
-    sum += word;
+  uint16_t sum;
+  if (!get_sum(rom, rom_end, &sum, fclose)) {
+    return 1;
   }
 
   printf("0x%04x\n", sum);
 
-defer_close_rom:
-  fclose(rom);
+  return 0;
+}
 
-defer_none:
-  return result;
+bool get_size(FILE *fp, int *result, defer_t on_error){
+  if (fseek(fp, 0, SEEK_END) != 0) {
+    perror("fseek 1");
+    on_error(fp);
+    return false;
+  }
+
+  *result = ftell(fp);
+  if (*result == -1) {
+    perror("ftell");
+    on_error(fp);
+    return false;
+  }
+
+  return true;
+}
+
+bool get_rom_end(FILE *fp, int size, int32_t *result, defer_t on_error) {
+  if (size < 0x1a8) {
+    fprintf(stderr, "error: size 0x%x is less than 0x1a8\n", size);
+    on_error(fp);
+    return false;
+  }
+
+  if (fseek(fp, 0x1a4, SEEK_SET) != 0) {
+    perror("fseek 2");
+    on_error(fp);
+    return false;
+  }
+
+  if (fread(result, 1, 4, fp) < 4) {
+    perror("fread");
+    on_error(fp);
+    return false;
+  }
+
+  *result = ntohl(*result) + 1;
+  if (*result > size) {
+    fprintf(stderr, "error: rom end 0x%x is beyond size 0x%x\n", *result, size);
+    on_error(fp);
+    return false;
+  }
+
+  return true;
+}
+
+bool get_sum(FILE *fp, int32_t rom_end, uint16_t *result, defer_t on_error) {
+  *result = 0;
+  for (int32_t i = 0x200; i < rom_end; i += 2) {
+    uint16_t word;
+    if (fseek(fp, i, SEEK_SET) != 0) {
+      perror("fseek");
+      on_error(fp);
+      return false;
+    }
+
+    if (fread(&word, 1, 2, fp) < 2) {
+      perror("fread");
+      on_error(fp);
+      return false;
+    }
+
+    word = ntohs(word);
+    *result += word;
+  }
+
+  return true;
 }
